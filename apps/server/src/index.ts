4781e7e4e7f93c1c4f31055fd5a6df15a688d371
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2025-10-28 21:33:08 +0530

    Fix bun hot reloading and add logging

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
+app.use(logger())
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Goodbye sailor')
 })
 
 export default app
